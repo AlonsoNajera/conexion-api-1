4a65769ef2fad027909062b9718be5280f984d58
AlonsoNajera <108036725+AlonsoNajera@users.noreply.github.com>
2022-09-17 11:27:09 -0500

    g

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 console.log("Entro al main.js");
+
+//FUNCION DE FLECHA
+// randomImage=()=> {
+
+
+// }
+
+const randomImage=()=> {
+    console.log("Entro a la Funcion randomImage");
+
+    fetch('https://picsum.photos/200/300')
+
+          
+            .then((data) => {
+                console.log(data);
+                document.getElementById('imgRandom').src=data.url;
+            })
+            .catch((error) => {
+                console.log("Ocurrio un error!");
+            });
+
+
+}
+
